b44205e3eeb0f71233dc8d03af70e7718a9c01bb
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-04-25 16:48:21 +0200

    automated quick commit

--- main.c
+++ main.c
@@ -3,12 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "buchalka/chararrchal.h"
+#include "buchalka/strfunchal.h"
 
 int main(int argc, char *argv[])
 {
-        int arr[] = {3, 2, 1};
-        for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) 
-                printf("%i\n", arr[i]);
-        return 0;
+	return 0;
 }
