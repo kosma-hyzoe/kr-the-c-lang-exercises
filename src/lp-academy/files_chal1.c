e095bf4165904dd79492055e428bceb36ccf6163
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-04-28 16:52:29 +0200

    automated quick commit

--- src/lp-academy/files_chal1.c
+++ src/lp-academy/files_chal1.c
@@ -12,7 +12,7 @@ int fgetsgetln(FILE *file)
     int ln = 1;
     char buffer[LINE_MAX];
     while (fgets(buffer, LINE_MAX, file) != NULL)
-	ln++;
+        ln++;
     return ln;
 }
 
@@ -20,8 +20,8 @@ int fgetcgetln(FILE *file)
 {
     int c, ln = 1;
     while ((c = fgetc(file)) != EOF)
-	if (c == '\n')
-	    ln++;
+        if (c == '\n')
+            ln++;
     return ln;
 }
 
@@ -29,27 +29,27 @@ void ftoupper(FILE *in, FILE *out)
 {
     int c;
     while ((c = fgetc(in)) != EOF)
-	islower(c) ? fputc(toupper(c), out) : fputc(c, out);
+        islower(c) ? fputc(toupper(c), out) : fputc(c, out);
 }
 
 int main(int argc, char *argv[])
 {
     char *infname;
     if (argc == 2) {
-	infname = argv[1];
+        infname = argv[1];
     } else {
-	infname = malloc(FILENAME_MAX);
-	printf("File name:\n");
-	fgets(infname, FILENAME_MAX, stdin);
-	infname[strcspn(infname, "\n")] = '\0';
+        infname = malloc(FILENAME_MAX);
+        printf("File name:\n");
+        fgets(infname, FILENAME_MAX, stdin);
+        infname[strcspn(infname, "\n")] = '\0';
     }
 
     FILE *i = fopen(infname, "r");
     FILE *o = fopen("tmp", "w+");
 
     if (!i || !o) {
-	printf("Cant open '%s'\n", !i ? infname : "out.txt");
-	return 1;
+        printf("Cant open '%s'\n", !i ? infname : "out.txt");
+        return 1;
     }
 
     ftoupper(i, o);
